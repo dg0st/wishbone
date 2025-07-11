efa07e32eb7aed88bd158ef14857e99320da2dab
Adam Lean <dmitrywork92@gmail.com>
2025-07-11 14:33:38 +0800

    .

--- wp-theme/frontpage.php
+++ wp-theme/frontpage.php
@@ -1,25 +1,25 @@
 <?php get_header(); ?>
 
 <section class="hero">
-        <div class="container">
-          <div class="hero-content">
-            <div class="hero-text">
-            <h2 class="subheading">Wishbone+Partners</h2>
-              <h1 class="heading">The home of<br />beautiful architecture.</h1>
-              <p class="description">
-                We are an architecture firm with a focus on beautiful but
-                functional design. At its heart, we believe design is about
-                usability and accessibility — these are the guiding principles
-                for our work. Read more about our previous projects, our process
-                and our team below.
-              </p>
-              <a href="#" class="read-more">Read more</a>
-            </div>
-            <div class="hero-image">
-              <img src="<?php echo get_template_directory_uri(); ?>./img/hero_image.png" alt="hero image" />
-            </div>
-          </div>
-        </div>
-      </section>
+  <div class="container">
+    <div class="hero-content">
+      <div class="hero-text">
+        <h2 class="subheading">Wishbone+Partners</h2>
+        <h1 class="heading">Родитель<br />красивой архитектуры.</h1>
+        <p class="description">
+          Мы — архитектурное бюро, специализирующееся на красивом, но
+          функциональном дизайне. В основе нашего подхода к дизайну лежат
+          удобство использования и доступность — это руководящие принципы
+          нашей работы. Подробнее о наших предыдущих проектах, нашем процессе
+          и нашей команде читайте ниже.
+        </p>
+        <a href="#" class="read-more">Read more</a>
+      </div>
+      <div class="hero-image">
+        <img src="<?php echo get_template_directory_uri(); ?>./img/hero_image.png" alt="hero image" />
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
